702ccd1dbb537d6e380d7df60d7ce2ca55cf3028
Elorri <etchemendy.elorri@gmail.com>
2016-03-09 19:15:21 +0100

    Add listActivity recyclerview

--- app/src/main/java/com/elorri/android/xyzreader/ArticleListFragment.java
+++ app/src/main/java/com/elorri/android/xyzreader/ArticleListFragment.java
@@ -3,9 +3,12 @@ package com.elorri.android.xyzreader;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Elorri on 09/03/2016.
@@ -16,7 +19,40 @@ public class ArticleListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_article_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        Adapter adapter = new Adapter();
+        recyclerView.setAdapter(adapter);
         return view;
 
+    }
+
+    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(
+                    getLayoutInflater(null).inflate(R.layout.list_item_article, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.text.setText("Bacon ipsum dolor amet pork belly meatball kevin spare ribs. Frankfurter swine corned beef meatloaf, strip steak.");
+        }
+
+        @Override
+        public int getItemCount() {
+            return 30;
+        }
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView text;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
     }
 }
